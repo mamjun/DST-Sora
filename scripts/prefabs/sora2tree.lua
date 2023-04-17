3212ac5c97c7af0ea7117f65de47ed392feff82a
MySora <mamjun1@qq.com>
2023-04-18 00:24:03 +0800

    v11.25.5 更新下版本号

--- scripts/prefabs/sora2tree.lua
+++ scripts/prefabs/sora2tree.lua
@@ -28,7 +28,7 @@ WeGame平台: 穹の空 模组ID：workshop-2199027653598519351
 3,严禁直接修改本mod内文件后二次发布。
 4,从本mod内提前的源码请保留版权信息,并且禁止加密、混淆。
 ]]
-local treeconfig = require "config/sora2tree"
+local treeconfig = require "soraconfig/sora2tree"
 
 require "prefabutil"
 
